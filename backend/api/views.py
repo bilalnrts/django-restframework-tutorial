db036f567beea2caf29a97ae6a144af7efa8557d
Bilal <bsalihnrts@gmail.com>
2023-11-18 03:22:37 +0300

    api now works with serializer

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,4 +1,18 @@
-from django.http import JsonResponse
+from django.forms.models import model_to_dict
+from products.models import Product
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from products.models import Product
+from products.serializers import ProductSerializer
 
+
+@api_view(['GET'])
 def api_home(request, *arg, **kwargs):
-    return JsonResponse({"message": "Hi there, this is your Django API response!!"})
+    instance = Product.objects.all().order_by('?').first()
+    data = {}
+    if instance:
+        # data = model_to_dict(instance, fields=['title', 'content', 'price', 'sale_price'])
+        data = ProductSerializer(instance).data
+    return Response(data)
+
+
